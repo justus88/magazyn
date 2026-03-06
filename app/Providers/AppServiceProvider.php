9018687f7e16cd46d129f39d1b43f1e522cc20cf
Mateusz <justusque@gmail.com>
2026-03-06 10:50:14 +0100

    Fix Livewire uploads behind Railway proxy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceRootUrl(config("app.url"));
         URL::forceScheme("https");
         //
     }
